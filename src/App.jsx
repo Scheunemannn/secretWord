83d0a62454746d2e58eb2880523861f65f7c3b77
Eduardo Scheunemann <dudu.jefe@hotmail.com>
2023-09-27 15:43:04 -0300

    adicionando muita logica

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { GameOver } from "./components/GameOver";
 import { useCallback, useState, useEffect } from "react";
 //data
 import { wordsList } from "./data/words";
-
+//cria uma variavel contendo os estagios do jogo: comeco, meio, fim
 const stages = [
   { id: 1, name: "start" },
   { id: 2, name: "game" },
@@ -21,48 +21,76 @@ function App() {
 
   const [words] = useState(wordsList);
 
-  const [pickedWord, setPickedWord] = useState("")
+  const [pickedWord, setPickedWord] = useState("");
 
-  const [pickedCategory, setPickedCategory] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("");
 
-  const [letters, setLetters] = useState([])
+  const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  const [guesses, setGuesses] = useState(3);
+
+  const [score, setScore] = useState(0);
+  //funcao para escolher palavra e categoria de palavra
   const pickWordAndCategory = () => {
-    const categories = Object.keys(words)
-    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
-    console.log(category)
-    const word = words[category][Math.floor(Math.random() * words[category].length)]
-    console.log(word)
+    //transforma o array de objetos (words) em uma variavel dentro da funcao
+    const categories = Object.keys(words);
+    //pega uma categoria aleatoria dentro daquele array de objetos, com o math random
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+    //pega uma palavra aleatoria dentro de uma categoria dentro do objeto words
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
 
-    return {word, category}
-  }
+    //transforma a palavra e a categoria que foram pegas em um objeto que e passado para outra funcao
+    return { word, category };
+  };
 
-//start the game
+  //start the game
   const startGame = () => {
-    const {word, category } = pickWordAndCategory()
-    let wordLetters = word.split("")
+    //puxa os objetos da função pickWordAndCategory
+    const { word, category } = pickWordAndCategory();
+    //separa cada letra da palavra escolhida
+    let wordLetters = word.split("");
+    //passa um map em cada letra da palavra escolhida e deixa todas em minuscula
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-    wordLetters = wordLetters.map((l) => l.toLowerCase())
-    console.log(word, category)
-    console.log(wordLetters)
-    setPickedWord(word)
-    setPickedCategory(category)
-    setLetters(letters)
+    console.log(word, category);
+    console.log(wordLetters);
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
     setGameStage(stages[1].name);
   };
-//process the letter input
-const verifyLetter =() => {
-  setGameStage(stages[2].name)
-}
-//goes to the start again
-const retry = () => {
-  setGameStage(stages[0].name)
-}
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+  //goes to the start again
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="global">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,51 @@
-import styles from "../styles/game.module.css"
-export const Game = ({verifyLetter}) => {
+/* eslint-disable react/prop-types */
+import styles from "../styles/game.module.css";
+export const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
     <div className={styles.game}>
-      <h1>dasdadadas</h1>
-      <button onClick={verifyLetter}>Voltar</button>
+      <p className={styles.points}>
+        <span>pontuacao {score}</span>
+      </p>
+      <h1>advinhe a palavra</h1>
+      <h3 className={styles.tip}>
+        dica da palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>voce ainda tem {guesses} tentativas(s)</p>
+      <div className={styles.wordContainer}>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letters) ? (
+            <span key={i} className={styles.letter}>
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className={styles.blankSquare}>
+              {" "}
+            </span>
+          )
+        )}
+      </div>
+      <div className={styles.letterContainer}>
+        <p>tente advinhar a letra</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button>jogar</button>
+        </form>
+      </div>
+      <div className={styles.wrongLetters}>
+        <p>letras ja utilizadas</p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter},</span>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
